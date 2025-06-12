54b0017a840550f98524750fc09e08cf94ad9183
Jun Shimura <j.shimura@sugawara.ac.jp>
2025-06-12 16:15:56 +0900

    Kyle

--- Assets/Fusion Tutorial/PlayerAvatar.cs
+++ Assets/Fusion Tutorial/PlayerAvatar.cs
@@ -8,10 +8,12 @@ public class PlayerAvatar : NetworkBehaviour
     public NetworkString<_16> NickName { get; set; }
 
     private NetworkCharacterController characterController;
+    private NetworkMecanimAnimator networkAnimator;
 
     public override void Spawned()
     {
         characterController = GetComponent<NetworkCharacterController>();
+        networkAnimator = GetComponentInChildren<NetworkMecanimAnimator>();
         var view = GetComponent<PlayerAvatarView>();
         // プレイヤー名をテキストに反映する
         view.SetNickName(NickName.Value);
@@ -27,12 +29,26 @@ public class PlayerAvatar : NetworkBehaviour
         // 移動
         var cameraRotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
         var inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        characterController.Move(inputDirection);
+        // characterController.Move(inputDirection);
         characterController.Move(cameraRotation * inputDirection);
         // ジャンプ
         if (Input.GetKey(KeyCode.Space))
         {
             characterController.Jump();
         }
+
+        // アニメーション（ここでは説明を簡単にするため、かなり大雑把な設定になっています）
+        var animator = networkAnimator.Animator;
+        var grounded = characterController.Grounded;
+        var vy = characterController.Velocity.y;
+        animator.SetFloat("Speed", characterController.Velocity.magnitude);
+        animator.SetBool("Jump", !grounded && vy > 4f);
+        animator.SetBool("Grounded", grounded);
+        animator.SetBool("FreeFall", !grounded && vy < -4f);
+        animator.SetFloat("MotionSpeed", 1f);
+
+
     }
+
+
 }
